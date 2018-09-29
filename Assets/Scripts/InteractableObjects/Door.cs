a2d1bf9a1fd4e5b18ca1e1e4e0ddb855c4d54fc2
Christian <bouwensec@gmail.com>
2018-09-29 13:31:16 -0400

    started on Door.cs and added in more assets.

---
+++ Assets/Scripts/InteractableObjects/Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public enum DoorType { teleporter, nextStage };
+
+    // Set these in Unity UI
+    public DoorType doorType; 
+    public Vector2 tpDest;
+
+    // TODO: do I need to add "override" to this
+	public void use()
+    {
+        print(name + " is being used");
+        // If the correct color player is using it
+        if (playerColor == color)
+        {
+            if (doorType == DoorType.teleporter)
+            {
+                print("teleporting player to tpDest: " + tpDest);
+                player.transform.position = tpDest;
+            }
+            else if (doorType == DoorType.nextStage)
+            {
+                print("moving player to next stage");
+            }
+
+            playerStats.points += points;
+        }
+    }
+}
