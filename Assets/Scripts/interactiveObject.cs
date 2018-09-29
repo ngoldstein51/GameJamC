39e194e5bd43694b9fe3563018a5fea7eff357c7
ngoldstein51 <ngoldstein51@gmail.com>
2018-09-29 13:09:41 -0400

    interactiveObject class

---
+++ Assets/Scripts/interactiveObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactiveObject : MonoBehaviour {
+    public Color color;
+    public interactiveObject target;
+    public bool active;
+
+    public interactiveObject(Color color,interactiveObject target, bool active)
+    {
+        this.color = color;
+        this.target = target;
+        this.active = active;
+    }
+
+    void use()
+    {
+        active = !active;
+    }
+}
