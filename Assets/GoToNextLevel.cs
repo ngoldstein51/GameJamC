4e532265eafdb50382371360eb8931036bfa82d1
Christian <bouwensec@gmail.com>
2018-09-29 23:39:29 -0400

    started on level 2.

---
+++ Assets/GoToNextLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GoToNextLevel : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+
+            // Do the animation
+            transform.parent.GetComponent<Animator>().SetBool("open", true);
+
+            // Go to the next level
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            
+        }
+    }
+
+}
