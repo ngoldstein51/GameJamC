bf1049feed720cbfc2f8938aedafb6414ad25ebd
Christian <bouwensec@gmail.com>
2018-09-30 12:08:41 -0400

    working on level 2

--- Assets/GoToNextLevel.cs
+++ Assets/GoToNextLevel.cs
@@ -11,7 +11,7 @@ public class GoToNextLevel : MonoBehaviour {
         {
 
             // Do the animation
-            transform.parent.GetComponent<Animator>().SetBool("open", true);
+            //transform.parent.GetComponent<Animator>().SetBool("open", true);
 
             // Go to the next level
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
